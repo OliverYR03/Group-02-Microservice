e4846f6ecf5a04ee7861597034d18777e5eb070d
OliverYR03 <oliver_del22@hotmail.com>
2023-07-06 22:07:10 -0500

    First Commit

---
+++ ms-admin-config-server/src/test/java/certus/edu/pe/msadminconfigserver/MsAdminConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package certus.edu.pe.msadminconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdminConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ms-admin-registry-server/src/test/java/certus/edu/pe/msadminregistryserver/MsadminregistryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package certus.edu.pe.msadminregistryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsadminregistryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
